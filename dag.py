88f49b2fb304d650b79cf0ec145f6156b38191e3
shhhQuiettt <krzysztof.skro@gmail.com>
2023-05-11 16:25:14 +0200

    Adjust to unlcear requirements

--- dag.py
+++ dag.py
@@ -2,55 +2,71 @@ import graphs
 import random
 
 
-def radnom_dag_adjacency_matrix(
-    vertex_numer: int, density: float
-) -> graphs.DirectedAdjacencyMatrix:
-    graph = graphs.DirectedAdjacencyMatrix(number_of_verticies=vertex_numer)
-    for i in range(vertex_numer):
-        for j in range(i + 1, vertex_numer):
-            if i != j:
-                if random.random() < density:
-                    graph.add_edge(i, j)
+def make_dag(g: graphs.Graph, saturation: float, order: int, seed=1024):
+    random.seed(seed)
 
-    return graph
+    for _ in range(order):
+        g.add_vertex()
 
+    for i in range(order):
+        for j in range(i + 1, order):
+            if random.random() < saturation:
+                g.add_edge(i, j)
 
-def random_dag_adjacency_list(
-    vertex_number: int, density: float
-) -> graphs.DirectedAdjacencyList:
-    adj_list = [[] for i in range(vertex_number)]
-    for i in range(vertex_number):
-        for j in range(i + 1, vertex_number):
-            if random.random() < density:
-                adj_list[i].append(j)
-    return graphs.DirectedAdjacencyList.from_list(adj_list)
+    return g
 
 
-def random_dag_forward_star(
-    vertex_number: int, density: float
-) -> graphs.DirectedForwardStar:
-    adj_list = [[] for i in range(vertex_number)]
-    for i in range(vertex_number):
-        for j in range(i + 1, vertex_number):
-            if random.random() < density:
-                adj_list[i].append(j)
-    return graphs.DirectedForwardStar.from_list(adj_list)
+make_dag(graphs.DirectedIncidenceMatrix(0), 0.1, 10).print()
+
+# def radnom_dag_adjacency_matrix(
+#     vertex_numer: int, density: float
+# ) -> graphs.DirectedAdjacencyMatrix:
+#     graph = graphs.DirectedAdjacencyMatrix(number_of_verticies=vertex_numer)
+#     for i in range(vertex_numer):
+#         for j in range(i + 1, vertex_numer):
+#             if i != j:
+#                 if random.random() < density:
+#                     graph.add_edge(i, j)
+
+#     return graph
 
 
-def random_dag_arc_list(vertex_number: int, density: float) -> graphs.DirectedArcList:
-    arc_list = []
-    for i in range(vertex_number):
-        for j in range(i + 1, vertex_number):
-            if random.random() < density:
-                arc_list.append((i, j))
-    return graphs.DirectedArcList.from_list(arc_list, vertex_number)
+# def random_dag_adjacency_list(
+#     vertex_number: int, density: float
+# ) -> graphs.DirectedAdjacencyList:
+#     adj_list = [[] for i in range(vertex_number)]
+#     for i in range(vertex_number):
+#         for j in range(i + 1, vertex_number):
+#             if random.random() < density:
+#                 adj_list[i].append(j)
+#     return graphs.DirectedAdjacencyList.from_list(adj_list)
 
 
-def random_dag_incidence_matrix(
-    num_vertices: int, density: float
-) -> graphs.DirectedIncidenceMatrix:
-    adj_list_graph = random_dag_adjacency_list(num_vertices, density)
+# def random_dag_forward_star(
+#     vertex_number: int, density: float
+# ) -> graphs.DirectedForwardStar:
+#     adj_list = [[] for i in range(vertex_number)]
+#     for i in range(vertex_number):
+#         for j in range(i + 1, vertex_number):
+#             if random.random() < density:
+#                 adj_list[i].append(j)
+#     return graphs.DirectedForwardStar.from_list(adj_list)
 
-    inc_matrix = graphs.adjacency_list_to_incidence_matrix(adj_list_graph.adj_list)
 
-    return graphs.DirectedIncidenceMatrix.from_matrix(inc_matrix, num_vertices)
+# def random_dag_arc_list(vertex_number: int, density: float) -> graphs.DirectedArcList:
+#     arc_list = []
+#     for i in range(vertex_number):
+#         for j in range(i + 1, vertex_number):
+#             if random.random() < density:
+#                 arc_list.append((i, j))
+#     return graphs.DirectedArcList.from_list(arc_list, vertex_number)
+
+
+# def random_dag_incidence_matrix(
+#     num_vertices: int, density: float
+# ) -> graphs.DirectedIncidenceMatrix:
+#     adj_list_graph = random_dag_adjacency_list(num_vertices, density)
+
+#     inc_matrix = graphs.adjacency_list_to_incidence_matrix(adj_list_graph.adj_list)
+
+#     return graphs.DirectedIncidenceMatrix.from_matrix(inc_matrix, num_vertices)
--- graphs.py
+++ graphs.py
@@ -6,7 +6,7 @@ from typing import Generator, Any
 
 class Graph(ABC):
     @abstractmethod
-    def add_vertex(self, v):
+    def add_vertex(self):
         pass
 
     @abstractmethod
@@ -45,16 +45,15 @@ class DirectedAdjacencyMatrix(Graph):
             [0 for _ in range(number_of_verticies)] for _ in range(number_of_verticies)
         ]
 
-    def add_vertex(self, v=None):
-        if v is not None:
-            raise NotImplementedError("AdjecencyMatrix does not support vertex labels")
-
+    def add_vertex(self):
+        self.number_of_verticies += 1
         self.matrix.append([0 for _ in range(len(self.matrix))])
         for row in self.matrix:
             row.append(0)
 
     def remove_vertex(self, v):
         self.matrix.pop(v)
+        self.number_of_verticies -= 1
         for row in self.matrix:
             row.pop(v)
 
@@ -89,6 +88,7 @@ class DirectedAdjacencyMatrix(Graph):
 class DirectedAdjacencyList(Graph):
     def __init__(self, number_of_verticies: int):
         self.adj_list = [[] for _ in range(number_of_verticies)]
+        self.number_of_verticies = number_of_verticies
 
     @classmethod
     def from_list(cls, adj_list: list):
@@ -96,13 +96,12 @@ class DirectedAdjacencyList(Graph):
         graph.adj_list = adj_list
         return graph
 
-    def add_vertex(self, v=None):
-        if v is not None:
-            raise NotImplementedError("AdjecencyList does not support vertex labels")
-
+    def add_vertex(self):
+        self.number_of_verticies += 1
         self.adj_list.append([])
 
     def remove_vertex(self, v):
+        self.number_of_verticies -= 1
         self.adj_list.pop(v)
 
     def add_edge(self, v1, v2):
@@ -132,10 +131,11 @@ class DirectedArcList(Graph):
         self.number_of_verticies = number_of_verticies
 
     def add_vertex(self, v=None):
-        raise NotImplementedError("ArcList does not support vertex adding")
+        self.number_of_verticies += 1
 
     def remove_vertex(self, v):
-        raise NotImplementedError("ArcList does not support vertex removal")
+        self.number_of_verticies -= 1
+        self.arc_list = [arc for arc in self.arc_list if arc[0] != v and arc[1] != v]
 
     def add_edge(self, v1, v2):
         self.arc_list.append((v1, v2))
@@ -171,13 +171,12 @@ class DirectedIncidenceMatrix(Graph):
         self.number_of_verticies = number_of_verticies
         self.matrix = [[] for _ in range(number_of_verticies)]
 
-    def add_vertex(self, v=None):
-        if v is not None:
-            raise NotImplementedError("IncidenceMatrix does not support vertex labels")
-
+    def add_vertex(self):
         self.matrix.append([])
+        self.number_of_verticies += 1
 
     def remove_vertex(self, v):
+        self.number_of_verticies -= 1
         self.matrix.pop(v)
 
     def add_edge(self, v1, v2):
@@ -232,10 +231,12 @@ class DirectedForwardStar(Graph):
         for i in range(number_of_verticies):
             self.forward_star[i] = {}
 
-    def add_vertex(self, v=None):
-        self.forward_star[v] = {}
+    def add_vertex(self):
+        self.forward_star[self.number_of_verticies] = {}
+        self.number_of_verticies += 1
 
     def remove_vertex(self, v):
+        self.number_of_verticies -= 1
         self.forward_star.pop(v)
 
     def add_edge(self, v1, v2):
